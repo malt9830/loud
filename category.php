b2a8670b10612d9ba56ff3fd59b6f547371f7e1c
malt9830 <70881301+malt9830@users.noreply.github.com>
2021-04-26 15:52:42 +0200

    kategorier virker :flushed: :point_right: :point_left:

--- category.php
+++ category.php
@@ -5,60 +5,67 @@
  * @subpackage Twenty_Twenty_One
  * @since Twenty Twenty-One 1.0
  */
-
 get_header();
 
 ?>
-
 <section id="popu_podcast"></section>
 
 <template>
-	<article class="pod_article">
-		<img class="pod_billede" src="" alt="">
-		<div class="pod_indhold">
-			<h3></h3>
-			<p class="kort_info"></p>
-		</div>
-	</article>
+    <article class="pod_article">
+        <img class="pod_billede" src="" alt="">
+        <div class="pod_indhold">
+            <h3></h3>
+            <p class="kort_info"></p>
+        </div>
+    </article>
 </template>
 
 
 <script>
-	let podcasts;
-	let categories;
-	const url = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/podcast?per_page=8";
-	const catUrl = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/categories";
+    let podcasts;
+    let categories;
+    const url = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/podcast?per_page=100";
+    const catUrl = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/categories";
 
-	async function getJson() {
-		const data = await fetch(url);
-		podcasts = await data.json();
-		console.log(podcasts);
 
-		const catdata = await fetch(catUrl);
-		categories = await catdata.json();
-		console.log(categories);
+    const pathname = window.location.pathname;
+    const catID = pathname.substring(pathname.lastIndexOf('category/') + 9).split('/').join('');
 
-		visPodcasts();
-	}
 
-	function visPodcasts() {
-		let template = document.querySelector("template");
-		let container = document.querySelector("#popu_podcast");
+    async function getJson() {
+        const data = await fetch(url);
+        podcasts = await data.json();
+        console.log(podcasts);
 
-		podcasts.forEach(podcast => {
-			let klon = template.cloneNode(true).content;
-			klon.querySelector("img").src = podcast.billede.guid;
-			klon.querySelector("div h3").innerHTML = podcast.title.rendered;
-			klon.querySelector("div .kort_info").innerHTML = podcast.kort;
-			klon.querySelector(".pod_article").addEventListener("click", () => {
-				location.href = podcast.link;
-			});
-			container.appendChild(klon);
+        const catdata = await fetch(catUrl);
+        categories = await catdata.json();
+        console.log(categories);
 
-		});
-	}
+        visPodcasts();
+    }
 
-	getJson();
+    function visPodcasts() {
+        let template = document.querySelector("template");
+        let container = document.querySelector("#popu_podcast");
+
+        console.log(catID);
+
+        podcasts.forEach(podcast => {
+            if (podcast.kategori == catID) {
+                let klon = template.cloneNode(true).content;
+                klon.querySelector("img").src = podcast.billede.guid;
+                klon.querySelector("div h3").innerHTML = podcast.title.rendered;
+                klon.querySelector("div .kort_info").innerHTML = podcast.kort;
+                klon.querySelector(".pod_article").addEventListener("click", () => {
+                    location.href = podcast.link;
+                });
+                container.appendChild(klon);
+            }
+
+        });
+    }
+
+    getJson();
 
 </script>
 
