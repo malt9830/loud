06eb998958bfbd9e38a539dee2354ce1517a8f83
elis2729 <Bassen7c>
2021-04-26 14:31:41 +0200

    kategori og podcastside

--- category.php
+++ category.php
@@ -8,18 +8,55 @@
 
 get_header();
 
-Test
+?>
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
+<section id="popu_podcast"></section>
 
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
+<template>
+	<article class="pod_article">
+		<img class="pod_billede" src="" alt="">
+		<div class="pod_indhold">
+			<h3></h3>
+			<p class="kort_info"></p>
+		</div>
+	</article>
+</template>
+
+<script>
+	let podcasts;
+	let aktuelcat = <?php echo get_the_ID() ?>;
+	const url = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/podcast?per_page=8";
+
+	async function getJson() {
+		const data = await fetch(url);
+		podcasts = await data.json();
+		console.log(podcasts);
+
+		visPodcasts();
 	}
-endwhile; // End of the loop.
 
+	function visPodcasts() {
+		let template = document.querySelector("template");
+		let container = document.querySelector("#popu_podcast");
+
+		podcasts.forEach(podcast => {
+			let klon = template.cloneNode(true).content;
+
+			klon.querySelector("img").src = podcast.billede.guid;
+			klon.querySelector("div h3").innerHTML = podcast.title.rendered;
+			klon.querySelector("div .kort_info").innerHTML = podcast.kort;
+			klon.querySelector(".pod_article").addEventListener("click", () => {
+				location.href = podcast.link;
+			});
+			container.appendChild(klon);
+
+		});
+	}
+
+	getJson();
+
+</script>
+
+<?php
 get_template_part( 'template-parts/footer/player' );
 get_footer();
