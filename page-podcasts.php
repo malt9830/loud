56cf370b885dc76b287f21761abfa579534861d0
elis2729 <Bassen7c>
2021-04-26 10:33:52 +0200

    kategorikanpper og kategoriside

--- page-podcasts.php
+++ page-podcasts.php
@@ -10,82 +10,86 @@ get_header();
 
 ?>
 <style>
-    /*-----------podcast-----------*/
+	/*-----------podcast-----------*/
 
-    #popu_podcast {
-        display: flex;
-        grid-gap: 20px;
-        padding: 10px 0px;
-    }
+	#popu_podcast {
+		display: flex;
+		grid-gap: 20px;
+		padding: 10px 0px;
+		margin-top: 0;
+	}
 
-    .pod_article {
-        flex-grow: 1;
-    }
+	.pod_article {
+		flex-grow: 1;
+	}
 
-    /*-----------kategorier-----------*/
+	/*-----------kategorier-----------*/
 
-    #kategorier {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-gap: 1.3rem;
-    }
+	#kategorier {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 1.3rem;
+	}
 
-    #kategorier button {
-        margin: 0 !important;
-    }
+	#kategorier button {
+		margin: 0 !important;
+	}
 
-    #kategorier button:nth-child(1) {
-        background: #DB0038 !important;
-    }
+	#kategorier button:nth-child(1) {
+		background: #DB0038 !important;
+	}
 
-    #kategorier button:nth-child(2),
-    #kategorier button:nth-child(4) {
-        background: #ED6A1D !important;
-    }
+	#kategorier button:nth-child(2),
+	#kategorier button:nth-child(4) {
+		background: #ED6A1D !important;
+	}
 
-    #kategorier button:nth-child(3),
-    #kategorier button:nth-child(7),
-    #kategorier button:nth-child(10) {
-        background: #F19C7A !important;
-    }
+	#kategorier button:nth-child(3),
+	#kategorier button:nth-child(7),
+	#kategorier button:nth-child(10) {
+		background: #F19C7A !important;
+	}
 
-    #kategorier button:nth-child(5),
-    #kategorier button:nth-child(8) {
-        background: #FABA42 !important;
-    }
+	#kategorier button:nth-child(5),
+	#kategorier button:nth-child(8) {
+		background: #FABA42 !important;
+	}
 
-    #kategorier button:nth-child(6),
-    #kategorier button:nth-child(9) {
-        background: #588B8B !important;
-    }
+	#kategorier button:nth-child(6),
+	#kategorier button:nth-child(9) {
+		background: #588B8B !important;
+	}
 
-    /*-----------mobil-----------*/
+	/*-----------mobil-----------*/
 
-    @media (max-width: 600px) {
+	@media (max-width: 600px) {
 
-        #popu_podcast {
-            overflow-x: scroll;
-            scroll-snap-type: x mandatory;
-        }
+		#popu_podcast {
+			overflow-x: scroll;
+			scroll-snap-type: x mandatory;
+		}
 
-        .pod_article {
-            flex-basis: 50%;
-            flex-shrink: 0;
-            scroll-snap-align: center;
-        }
+		.pod_article {
+			flex-basis: 50%;
+			flex-shrink: 0;
+			scroll-snap-align: center;
+		}
 
-    }
+		.wp-block-columns .wp-block-column>* {}
+
+
+	}
 
 </style>
 
 <template>
-    <article class="pod_article">
-        <img class="pod_billede" src="" alt="">
-        <div class="pod_indhold">
-            <h3></h3>
-            <p class="kort_info"></p>
-        </div>
-    </article>
+	<article class="pod_article">
+		<img class="pod_billede" src="" alt="">
+		<div class="pod_indhold">
+			<h3></h3>
+			<p class="kort_info"></p>
+		</div>
+	</article>
 </template>
 
 <!--
@@ -113,57 +117,67 @@ get_header();
 -->
 
 <script>
-    let podcasts;
-    let categories;
-    const url = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/podcast?per_page=8";
-    const catUrl = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/categories";
+	let podcasts;
+	let categories;
+	const url = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/podcast?per_page=8";
+	const catUrl = "https://malthekusk.one/kea/loud/wordpress/wp-json/wp/v2/categories";
 
-    async function getJson() {
-        const data = await fetch(url);
-        podcasts = await data.json();
-        console.log(podcasts);
+	async function getJson() {
+		const data = await fetch(url);
+		podcasts = await data.json();
+		console.log(podcasts);
 
-        const catdata = await fetch(catUrl);
-        categories = await catdata.json();
-        console.log(categories);
+		const catdata = await fetch(catUrl);
+		categories = await catdata.json();
+		console.log(categories);
 
-        visPodcasts();
-        opretKnapper();
-    }
+		visPodcasts();
+		opretKnapper();
+	}
 
-    function opretKnapper() {
-        categories.forEach(cat => {
-            document.querySelector("#kategorier").innerHTML += `<button class="custom-button" = "${cat.id}">${cat.name}</button>`;
-        });
+	function opretKnapper() {
+		categories.forEach(cat => {
+			document.querySelector("#kategorier").innerHTML += `<button class="custom-button" data-podcast="${cat.id}">${cat.name}</button>`;
+		});
 
-    }
+		addEventListenersToButtons();
+
+	}
+
+	function addEventListenersToButtons() {
+		document.querySelectorAll("#kategorier button").forEach(elm => {
+			elm.addEventListener("click", () => {
+				location.href = elm.link;
+			});
+		})
+	}
 
 
-    function visPodcasts() {
-        let template = document.querySelector("template");
-        let container = document.querySelector("#popu_podcast");
+	function visPodcasts() {
+		let template = document.querySelector("template");
+		let container = document.querySelector("#popu_podcast");
 
-        podcasts.forEach(podcast => {
-            let klon = template.cloneNode(true).content;
+		podcasts.forEach(podcast => {
+			let klon = template.cloneNode(true).content;
 
 
-            //				let stream = podcast.streaming
-            //				stream.sort(function(a, b) {
-            //					return b - a;
-            //				});
+			//				let stream = podcast.streaming
+			//				stream.sort(function(a, b) {
+			//					return b - a;
+			//				});
 
-            klon.querySelector("img").src = podcast.billede.guid;
-            klon.querySelector("div h3").innerHTML = podcast.title.rendered;
-            klon.querySelector("div .kort_info").innerHTML = podcast.kort;
-            klon.querySelector(".pod_article").addEventListener("click", () => {
-                location.href = podcast.link;
-            });
-            container.appendChild(klon);
+			klon.querySelector("img").src = podcast.billede.guid;
+			klon.querySelector("div h3").innerHTML = podcast.title.rendered;
+			klon.querySelector("div .kort_info").innerHTML = podcast.kort;
+			klon.querySelector(".pod_article").addEventListener("click", () => {
+				location.href = podcast.link;
+			});
+			container.appendChild(klon);
 
-        });
-    }
+		});
+	}
 
-    getJson();
+	getJson();
 
 </script>
 
